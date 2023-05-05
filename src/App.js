4839fabf1f5d94e98a3a634e01c3a4ff6a918ede
Tracy Zhao <tracyzhao24@g.ucla.edu>
2023-05-04 21:59:04 -0700

    all stories array

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import Footer from './components/Footer';
 
 function App() {
   const [ data, setData ] = useState(null);
+  var all_stories = [];
   
   useEffect(() => {
 		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/spring-sing-2023")
 		.then(res => res.json())
 		.then(res => setData(res.data['article.aml']))
   }, [])
+
+  if (data)
+  {
+    all_stories = data.shelf_stories.concat(data.right_stories_1).concat(data.middle_stories).concat(data.left_stories).concat(data.right_stories_2)
+    console.log(all_stories)
+  }
+  
 
   return data && (
     <div className="App">
