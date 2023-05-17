6c041b86c7c462f4f68d0cd363ede11b2bb02a79
tsakshi011 <tsakshi01@gmail.com>
2023-05-16 17:35:51 -0700

    Plant Shelf Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
+import PlantShelf from './components/PlantShelf';
 import Footer from './components/Footer';
 
 function App() {
@@ -19,12 +20,11 @@ function App() {
     all_stories = data.shelf_stories.concat(data.right_stories_1).concat(data.middle_stories).concat(data.left_stories).concat(data.right_stories_2)
     console.log(all_stories)
   }
-  
-
   return data && (
     <div className="App">
       <Header/>
       Hello Daily Bruin!
+      <PlantShelf articles={data.shelf_stories}/>
       <Footer/>
     </div>
   );
---
+++ src/components/PlantShelf.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+import { mediaQueries } from "../shared/config";
+import Plant from "../images/plant.svg";
+import ArticleCard from "./ArticleCard.js";
+
+const Container = styled.div`
+    max-width: 100vw;
+    position: relative;
+    overflow: visible;
+    margin-top: 120em;
+    margin-bottom: 225.5625em;
+`
+
+const Grid = styled.div`
+  display: flex;
+  width: 55em;
+  grid-template-columns: repeat(2, 1fr);
+  overflow: visible;
+  margin-bottom: -20.116em;
+  margin-left: 4em;
+
+  ${mediaQueries.mobile} {
+    display: flex;
+    flex-direction: column;
+    width: 90vw;
+    margin: auto;
+    align-items: center;
+  }
+`;
+
+const Shelf = styled.div`
+  width: 80%;
+  height: 2.25em;
+  margin-left: -0.01953125em;
+  background: #C08E5F;
+  border: 2px solid #866B52;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
+
+/*<img style={{"width": "20%", "float":"left"}} src={LeftDivider}/>
+//margin-top: 2035px;
+
+*/
+//<h1>PLANT SHELF</h1>
+
+export default function PlantShelf(props) {
+  return (
+  <>     
+      <Container>
+            <Grid>
+            {props && props.articles
+                  ? props.articles.map((item) => {
+                      return (
+                          <ArticleCard
+                          article_float = "green"
+                          article_title={item.article_title}
+                          article_byline={item.article_byline}
+                          article_image={item.article_image}
+                          article_url={item.article_url}
+                          />
+                      );
+                  })
+              : null}
+            </Grid>
+            <Shelf>
+            <h1></h1>
+            <img style={{"width": "15em", "float":"right", "margin-bottom": "-1.25em"}} src={Plant}/>
+          </Shelf>
+        </Container> 
+  </>
+  );
+}
